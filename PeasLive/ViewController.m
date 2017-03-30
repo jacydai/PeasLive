7111fceaba82596bff8dccc71de531861aa25a80
dxs <dxs@91dida.cn>
2017-03-30 22:20:00 +0800

    Initialized project

---
+++ PeasLive/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PeasLive
+//
+//  Created by daixingsi on 28/03/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ PeasLive/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PeasLive
+//
+//  Created by daixingsi on 28/03/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
