e658ebf2b0831af51d09b08774ab93ef2324b962
jacydai <jacydai@gmail.com>
2017-03-31 17:27:43 +0800

    Create module view controller.

---
+++ PeasLive/Classes/Module/Live/DXSLiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  DXSLiveViewController.h
+//  PeasLive
+//
+//  Created by daixingsi on 31/03/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import "DXSBaseViewController.h"
+
+@interface DXSLiveViewController : DXSBaseViewController
+
+@end
---
+++ PeasLive/Classes/Module/Profile/DXSProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  DXSProfileViewController.h
+//  PeasLive
+//
+//  Created by daixingsi on 31/03/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import "DXSBaseViewController.h"
+
+@interface DXSProfileViewController : DXSBaseViewController
+
+@end
