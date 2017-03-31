393812398ebbf9e4877c3f06d9ee8ec8cd2c1210
jacydai <jacydai@gmail.com>
2017-03-31 17:21:11 +0800

    Create  base controller

---
+++ PeasLive/Classes/Module/Base/DXSBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  DXSBaseViewController.h
+//  PeasLive
+//
+//  Created by daixingsi on 31/03/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXSBaseViewController : UIViewController
+
+@end
---
+++ PeasLive/Classes/Module/Base/DXSNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  DXSNavigationController.h
+//  PeasLive
+//
+//  Created by daixingsi on 31/03/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXSNavigationController : UINavigationController
+
+@end
