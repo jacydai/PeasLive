697c7770950be6ae9a1428818ab4af5d975ccab6
jacydai <jacydai@gmail.com>
2017-04-16 11:24:45 +0800

    Add tools catageory.

---
+++ PeasLive/Classes/Common/NSObject+JADTools.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+JADTools.h
+//  PeasLive
+//
+//  Created by daixingsi on 16/04/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (JADTools)
+
+@end
---
+++ PeasLive/Classes/Common/NSObject+JADTools.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+JADTools.m
+//  PeasLive
+//
+//  Created by daixingsi on 16/04/2017.
+//  Copyright © 2017 NextStep.Inc. All rights reserved.
+//
+
+#import "NSObject+JADTools.h"
+
+@implementation NSObject (JADTools)
+
+@end
